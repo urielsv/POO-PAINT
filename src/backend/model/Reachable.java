982aa323500e2e3df6381c7eb319e46cff7404a3
Uriel Sosa Vazquez <urielsosavazquez@gmail.com>
2024-01-11 19:29:26 -0300

    chore: changes by luki

--- src/backend/model/Reachable.java
+++ src/backend/model/Reachable.java
@@ -3,6 +3,6 @@ package backend.model;
 public interface Reachable {
 
     boolean isReachable(Point selection);
-    boolean isReachable(Rectangle selection);
+    boolean isContained(Rectangle selection);
 
 }
